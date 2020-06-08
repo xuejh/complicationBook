69127f49e942b994c42f5b9029c8479c1cf36c39
871030 <871030@nd.com>
2020-06-08 10:54:02 +0800

    C++初始化文件

---
+++ 02/C++/02/02/SimpleLexer.cpp
@@ -0,0 +1,22 @@
+//
+//  SimpleLexer.cpp
+//  02
+//
+//  Created by XJH on 2020/6/8.
+//  Copyright © 2020 XJH. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class SimpleLexer {
+private:
+    string tokenText;
+    std::vector<string> tokens;
+    
+public:
+    
+};
---
+++ 02/C++/02/02/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  02
+//
+//  Created by XJH on 2020/6/8.
+//  Copyright © 2020 XJH. All rights reserved.
+//
+
+#include <iostream>
+#include "SimpleLexer.cpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    SimpleLexer *s = new SimpleLexer();
+    std::cout << s<<endl;
+    return 0;
+}
